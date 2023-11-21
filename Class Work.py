7def3094afa865c45aff220614980712218dc03e
Dundy13 <150956204+Dundy13@users.noreply.github.com>
2023-11-21 12:01:56 +0200

    Add files via upload

---
+++ Class Work.py
@@ -0,0 +1,33 @@
+a = 55
+b = 5
+print(a - b)
+
+
+a = 13
+b = 5
+print(a * b)
+
+a = 18
+b = 3
+print (a / b)
+
+a = 101
+b = 13
+print (a //b )
+
+
+str1 = 'Kogato bqh ovcharche'
+str2 = 'i ovcite pasqh'
+print(str1 + str2)
+
+
+str1 = 'Every begining is tough'
+str2 = 'Until you do not start'
+#print(str1 + ' ' + str2)
+
+test_array = [2, 3, 5, 8, 13]
+print(test_array[4])
+
+
+dict_array = {a:False, b:'MD Basketball', c:'They call me big papa'}
+print(dict_array [c])
