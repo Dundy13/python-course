740302703a5cf8938164d187849b8cc6ca1fe456
Dundy13 <150956204+Dundy13@users.noreply.github.com>
2023-12-04 10:03:30 +0200

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,11 @@
+#w = 15 #intiger
+#q = 7.43 #float point
+
+
+#print(w // q)
+
+first_name = "Daniel"
+middle_name = "Lachezarov"
+last_name = "Gyokov"
+full_name = (first_name + " " + middle_name + " " + last_name)
+print(full_name)
